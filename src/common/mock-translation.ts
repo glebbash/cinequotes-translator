a41b888889cf3a1f901e1a79951b952a86f9ea16
glebbash <glebbash@gmail.com>
2020-12-02 20:10:29 +0100

    Add translation mock

---
+++ src/common/mock-translation.ts
@@ -0,0 +1,31 @@
+import { TranslationServiceClient } from '@google-cloud/translate'
+
+const mockedTranslations = {
+    'Say hello to my little friend.': 'Dis bonjour à mon petit ami.',
+    "Toto, I've got a feeling we're not in Kansas anymore.":
+        "Toto, j'ai le sentiment que nous ne sommes plus au Kansas.",
+    'May the force be with you.': 'Que la force soit avec toi.',
+    'You talking to me?': 'Tu me parle?',
+    'You had me at hello.': 'Vous me avez eu à bonjour.',
+}
+
+function translate(text: string) {
+    const translatedText = mockedTranslations[text]
+    if (translatedText === undefined) {
+        throw new Error('Oops')
+    }
+    return {
+        translations: [
+            {
+                detectedSourceLanguage: 'en',
+                translatedText,
+            },
+        ],
+    }
+}
+
+export function mockTranslation() {
+    TranslationServiceClient.prototype.translateText = async ({ contents }) => {
+        return translate(contents) as any
+    }
+}
