9ac1df64adb76b46345ae2462c195310c3ddcb69
glebbash <glebbash@gmail.com>
2020-12-04 15:18:07 +0100

    Update firestore service

--- src/firestore/firestore.service.ts
+++ src/firestore/firestore.service.ts
@@ -1,7 +1,9 @@
-import { Injectable } from '@nestjs/common'
 import { Firestore } from '@google-cloud/firestore'
+import { Injectable } from '@nestjs/common'
 
 @Injectable()
 export class FirestoreService {
-    db = new Firestore({ projectId: 'dummy' })
+    db = new Firestore({
+        projectId: process.env.FIRESTORE_PROJECT_ID ?? 'dummy-test',
+    })
 }
