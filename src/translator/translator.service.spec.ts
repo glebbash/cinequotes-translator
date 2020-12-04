ea8e8e60375c5ad640cfe5728973edb51de7ae8e
glebbash <glebbash@gmail.com>
2020-12-04 15:38:27 +0100

    Add translator service unit tests

--- src/translator/translator.service.spec.ts
+++ src/translator/translator.service.spec.ts
@@ -1,18 +1,55 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { TranslatorService } from './translator.service';
+import { TranslationServiceClient } from '@google-cloud/translate'
+import { Test, TestingModule } from '@nestjs/testing'
+import { TranslatorService } from './translator.service'
+
+jest.mock('@google-cloud/translate')
 
 describe('TranslatorService', () => {
-  let service: TranslatorService;
+    let service: TranslatorService
+    let googleTranslator: TranslationServiceClient
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [TranslatorService],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [
+                {
+                    provide: 'TRANSLATOR_CLIENT',
+                    useClass: TranslationServiceClient,
+                },
+                TranslatorService,
+            ],
+        }).compile()
 
-    service = module.get<TranslatorService>(TranslatorService);
-  });
+        service = module.get(TranslatorService)
+        googleTranslator = module.get('TRANSLATOR_CLIENT')
+    })
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
-});
+    it(`calls google translate api with correct params
+        and returns string`, async (done) => {
+        const enQuote = 'May the force be with you.'
+        const mockFrQuote = 'Que la force soit avec toi.'
+        const translateRequest = {
+            contents: [enQuote],
+            sourceLanguageCode: 'en',
+            targetLanguageCode: 'fr',
+        }
+        const translateResponse = [
+            {
+                translations: [
+                    {
+                        translatedText: mockFrQuote,
+                    },
+                ],
+            },
+        ]
+
+        const translateTextSpy = jest
+            .spyOn(googleTranslator, 'translateText')
+            .mockResolvedValue(translateResponse as never)
+
+        const frQuote = await service.translate(enQuote, 'en', 'fr')
+
+        expect(translateTextSpy).toBeCalledWith(translateRequest)
+        expect(frQuote).toEqual(mockFrQuote)
+        done()
+    })
+})
